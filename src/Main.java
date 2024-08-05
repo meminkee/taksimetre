304d3f72921eeec645b0e2a90a4339a1b1b80fc3
Mehmet Emin Kart <meminkee.05@gmail.com>
2024-08-05 21:25:16 +0300

    düzenlendi

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Değişkenler Belirlendi.
+
         int km;
         double perKm = 2.20, total,startPrice = 10;
         Scanner inp = new Scanner(System.in);
 
+        //Değişken istenildi.
+
         System.out.print("Mesafeyi Km Cinsinden Giriniz : ");
         km = inp.nextInt();
 
+        //İşlem
+
         total = (km*perKm);
         total+= (startPrice);
-
         total = (total <20) ? 20 : total;
 
-
-        System.out.print("Toplam Tutarınız : "+total);
-
+        //Sonuç
 
 
+        System.out.print("Toplam Ödenecek Tutarınız : "+total);
 
     }
 }
